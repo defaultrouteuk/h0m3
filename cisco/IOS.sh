7adc23184887ed7a6147406915e55dcbce49f739
Richard Lowton <18391599+defaultrouteuk@users.noreply.github.com>
2022-05-23 18:19:37 +0400

    Removed NTP$n loop
    It was easier to create the function then call the function rather than list each NTP line individually.

--- cisco/IOS.sh
+++ cisco/IOS.sh
@@ -3,22 +3,16 @@
 # Use NTPx environment value to add new addresses to existing ACL
 
 let n=0
-for ntp in `show ntp ass | cut -f1 -d 'address' | cut -f1 -d 'sys.peer' | cut -c'3-16'` 
-do 
- NTP$n=$ntp
- let n++
-done
 
-# Function to configure existing access-list
 function set_ntp_acl () {
-    configure terminal
-     ip access-list standard ACL-NTP_PEERS
-            permit $NTP0
-            permit $NTP1
-            permit $NTP2
-            permit $NTP3
-    end
+ configure terminal
+  ip access-list standard ACL-NTP_PEERS
+   permit host $ntp
+   end
 }
 
-# Run the function
-set_ntp_acl
+for ntp in `show ntp ass | cut -f1 -d 'address' | cut -f1 -d 'sys.peer' | cut -c'3-16'` 
+do 
+ set_ntp_acl
+ let n++
+done
